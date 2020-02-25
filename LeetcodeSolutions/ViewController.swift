7d1539fb9454a5bb1a9e0f2a7a4a2b75fadd1de1
Anna Miksiuk <miksiukanna@gmail.com>
2020-02-25 08:27:01 +0300

    Initial commit

---
+++ LeetcodeSolutions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeetcodeSolutions
+//
+//  Created by Anna Miksiuk on 2/25/20.
+//  Copyright © 2020 Anna Miksiuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LeetcodeSolutionsTests/SolutionTwoSumTests.swift
@@ -0,0 +1,9 @@
+//
+//  SolutionTwoSumTests.swift
+//  LeetcodeSolutionsTests
+//
+//  Created by Anna Miksiuk on 2/25/20.
+//  Copyright © 2020 Anna Miksiuk. All rights reserved.
+//
+
+import Foundation
